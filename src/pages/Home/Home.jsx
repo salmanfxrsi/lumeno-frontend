ced117f9ad1e1e24e88ed1ed5334850c78674dda
salmanfxrsi <salmanxprivate@gmail.com>
2025-01-13 21:15:19 +0600

    feat: initialize Lumeno frontend environment - Set up React with React Router, Tailwind CSS, and DaisyUI. - Created folder for static resources. - Add assets

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import { Helmet } from "react-helmet-async";
+
+const Home = () => {
+    return (
+        <div>
+            <Helmet><title>Home | Lumeno</title></Helmet>
+        </div>
+    );
+};
+
+export default Home;
