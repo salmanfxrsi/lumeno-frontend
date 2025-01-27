9b144138149b964d9a0b7b1dfa784d7a81929d7a
salmanfxrsi <salmanxprivate@gmail.com>
2025-01-27 17:08:26 +0600

    feat: implement useAxiosSecure hook with interceptors for request authorization and error handling

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,37 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
 const useAxiosSecure = () => {
+  const navigate = useNavigate()
+  const { signOut } = useAuth()
+
+  // request interceptor to add authorization header for every secure call to the api
+  axiosSecure.interceptors.request.use(function (config) {
+    const token = localStorage.getItem('access-token')
+    config.headers.authorization = `Bearer ${token}`
+    return config;
+  }, function (error) {
+    return Promise.reject(error)
+  })
+
+
+  // intercepts 401 and 403 status
+  axiosSecure.interceptors.response.use(function (response) {
+    return response;
+  }, async (error) => {
+    const status = error.response.status;
+    if (status === 401 || status === 403) {
+      await signOut()
+      navigate('/login')
+    }
+    return Promise.reject(error)
+  })
+
   return axiosSecure;
 };
 
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -91,7 +91,7 @@ const AuthProvider = ({ children }) => {
     return () => {
       return unsubscribe();
     };
-  }, []);
+  }, [axiosPublic]);
 
   const authInfo = {
     user,
